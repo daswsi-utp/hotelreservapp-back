9c44a5a339e9f0c08c54e5ff62c48b7450dd302a
Haem807 <phildaxko@gmail.com>
2025-05-28 04:06:00 -0500

    Adding user and global exception folder and files

---
+++ reservas/src/main/java/com/hotel/reservas/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.hotel.reservations.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //all exceptions handler
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
---
+++ reservas/src/main/java/com/hotel/reservas/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.hotel.reservations.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
