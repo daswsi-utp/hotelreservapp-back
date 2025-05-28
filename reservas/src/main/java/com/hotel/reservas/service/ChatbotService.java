55ef8c0ec87606b7811453152ae5672a1ebab2eb
Haem807 <phildaxko@gmail.com>
2025-05-28 03:42:27 -0500

    adding user data access object

--- reservas/src/main/java/com/hotel/reservas/service/ChatbotService.java
+++ reservas/src/main/java/com/hotel/reservas/service/ChatbotService.java
@@ -8,8 +8,12 @@ public class ChatbotService {
     public String procesarMensaje(String mensaje) {
         if (mensaje.toLowerCase().contains("hola") || mensaje.toLowerCase().contains("buenos días")) {
             return "¡Hola! ¿En qué puedo ayudarte hoy?";
-        } else if (mensaje.toLowerCase().contains("precio")) {
-            return "El precio de una habitación estándar es $100 por noche.";
+        } else if (mensaje.toLowerCase().contains("precio estandar")) {
+            return "El precio de una habitación estándar es S/.100 ó $25 por noche.";
+        else if (mensaje.toLowerCase().contains("precio matrimonial")) {
+                return "El precio de una habitación matrimonial es S/.150 ó $40 por noche.";
+        else if (mensaje.toLowerCase().contains("precio deluxe")) {
+                    return "El precio de una habitación deluxe es S/.200 ó $50 por noche.";
         } else if (mensaje.toLowerCase().contains("disponibilidad")) {
             return "¿Para qué fecha te gustaría hacer la reserva?";
         } else {
