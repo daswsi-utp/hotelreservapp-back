e2c4792bea2407b01e4e97e012e1f13eb8f690ea
winsthonapazamamani <winsthonapazamamani@gmail.com>
2025-05-29 11:57:22 -0500

    add dto

--- src/main/java/com/hotel/backend/dto/AuthResponse.java
+++ src/main/java/com/hotel/backend/dto/AuthResponse.java
@@ -1,2 +1,17 @@
-package com.hotel.backend.dto;public class AuthResponse {
+package com.hotel.backend.dto;
+
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
